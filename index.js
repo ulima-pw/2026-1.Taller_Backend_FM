7fc64028525a147173bd0ab699a8223ef9b64603
ProfO1602 <ProfO1602@lab.aca>
2026-07-10 12:14:09 -0500

    sacando .env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { PrismaPg } from "@prisma/adapter-pg"
 import 'dotenv/config'
 
 const app = express()
-const PORT = 5000
+const PORT = process.env["PORT"] | 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
